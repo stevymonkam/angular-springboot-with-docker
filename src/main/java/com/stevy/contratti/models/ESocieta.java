16e570c2871b404f1c393bd59a4bc83fdfe5276b
stevy monkam <stevy.monkam@yahoo.fr>
2024-05-06 21:59:05 +0200

    firt8

--- src/main/java/com/stevy/contratti/models/ESocieta.java
+++
@@ -1,9 +0,0 @@
-package com.stevy.contratti.models;
-
-public enum ESocieta {
-     BE_CONSULTING,
-     BE_SOLUTIONS,
-     BE_BE,
-     IBE,
-     BE_SFCS,
-}
--- src/main/java/com/stevy/contratti/repository/FileContratRepository.java
+++
@@ -1,9 +0,0 @@
-package com.stevy.contratti.repository;
-
-
-import com.stevy.contratti.models.FileContrat;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FileContratRepository extends JpaRepository<FileContrat, Long> {
-    public FileContrat findFileContratById(Long id);
-}
